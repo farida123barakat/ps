75094f061fa87ea1b697b04515dae95290fcffd6
Farida Barakat <faridabarakat@Faridas-MacBook-Air.local>
2026-01-15 15:18:15 +0200

    change

--- sp.py
+++ sp.py
@@ -1,1 +1,7 @@
-print("test docker")
+name = input("Enter your name: ")
+
+print("Hello,", name)
+print("Welcome to this simple Python app.")
+
+length = len(name)
+print("Your name has", length, "characters.")
