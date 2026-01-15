507ff00257a220018d250577628c943eeab18733
Farida Barakat <faridabarakat@Faridas-MacBook-Air.local>
2026-01-15 15:14:43 +0200

    i changed

--- sp.py
+++ sp.py
@@ -1,7 +1,1 @@
-name = input("Enter your name: ")
-
-print("Hello,", name)
-print("Welcome to this simple Python app.")
-
-length = len(name)
-print("Your name has", length, "characters.")
+print("test docker")
